6a95eb30f41c1f7318e23f14a700dc8216adb44e
wangershuai <wanganlou@gmail.com>
2022-10-28 19:26:22 +0800

    初始化工程

--- lib/controller/chat_message_controller.dart
+++
@@ -1,7 +0,0 @@
-import 'package:chat/model/message_data.dart';
-import 'package:get/get.dart';
-
-// 用于状态管理
-class ChatMessageConrtoller extends GetxController {
-  var chatMessage = <MessageData>[].obs;
-}
